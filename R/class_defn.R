013edacdb754b50591b90d5b1e6b3a6aacffe89c
suziebrown <suzie.brown42@gmail.com>
2018-04-23 10:54:45 +0100

    added attributes to genealogy; created subclass samplegenealogy

--- R/class_defn.R
+++ R/class_defn.R
@@ -1,1 +1,2 @@
-setClass("genealogy",representation(history="matrix", model="character"))
+setClass("genealogy",representation(history="matrix", N="integer", Ngen="integer", model="character"))
+setClass("samplegenealogy", representation(ancestry="matrix", samplesize="integer", sample="integer", MRCA="integer"),contains="genealogy")
