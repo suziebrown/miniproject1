a53058f422e9e1c12e76bed367e079faadf911bf
suziebrown <suzie.brown42@gmail.com>
2018-04-23 15:07:19 +0100

    Added "maxgen" functionality to stop searching above a certain number of generations

--- R/trace_ancestry.R
+++ R/trace_ancestry.R
@@ -4,6 +4,7 @@
 #'
 #' @param history an ocject of class genealogy, as returned from WFsim or moranSim
 #' @param sampl a vector of integers indicating which individuals from the most recent generation are to be included in the sample
+#' @param maxgen the maximum number of generations back in time to investigate
 #'
 #' @return an object of class samplegenealogy
 #'
@@ -12,29 +13,30 @@
 #' @export traceAncestry
 #'
 
-traceAncestry <- function(history, sampl){
+traceAncestry <- function(history, sampl, maxgen=NULL){
   n <- length(sampl)
   N <- attr(history, 'N')
-  N.gen <- attr(history, 'Ngen')
+  N.gen <- (attr(history, 'Ngen'))
+  min.gen <- max(1, N.gen-maxgen)
 
   ancestry <- matrix(NA, nrow=N.gen, ncol=n)
   ancestry[N.gen,] <- sort(sampl)
   MRCA <- NA
 
-  for (t in ((N.gen-1):1)){ ## try eleiminating some loops
+  for (t in ((N.gen-1):min.gen)){ ## try eliminating the for loops
     for (i in 1:n){
       ancestry[t,i] <- history[t,ancestry[t+1,i]]
     }
     if (is.na(MRCA)){
       if (length(unique(ancestry[t,]))==1){
-        MRCA <- t
+        MRCA <- t ## could leave NAs above once MRCA is found?
       }
     }
   }
   class(ancestry) <- 'samplegenealogy'
   ancestry@samplesize <- n
   ancestry@sample <- sampl
-  ancestry@MRCA <- MRCA
+  ancestry@MRCA <- as.integer(MRCA)
   ancestry@history <- unclass(history)
   ancestry
 }
