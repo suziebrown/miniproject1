4e5e1cec63756bcd24b42024d3ba755f442a41cd
suziebrown <suzie.brown42@gmail.com>
2018-05-08 15:00:18 +0100

    standard SMC algm debugged - che stupida!

--- R/toydata.R
+++ R/toydata.R
@@ -1,6 +1,7 @@
 Delta <- 0.1
 sigma <- 0.1
 tmax <- 15
+N <- 50
 
 ## discretised Ornstein-Uhlenbeck process
 
@@ -10,28 +11,22 @@ sim.data <- function(tmax, Delta, sigma){
   x[1] <- rnorm(1)
   y[1] <- rnorm(1,x[1], sigma)
   for (t in 2:(tmax+1)){
-    x[t] <- (1-Delta)*x[t-1] + Delta^(-0.5)*rnorm(1)
+    x[t] <- rnorm(1, (1-Delta)*x[t-1], Delta^(0.5))
     y[t] <- rnorm(1,x[t],sigma)
   }
   list(x=x,y=y)
 }
 
 K <- function(x){ ## transition function of HMM
-  (1-Delta)*x + Delta^(0.5)*rnorm(1)
+  (1-Delta)*x + Delta^(0.5)*rnorm(length(x))
 }
 
-p <- function(x,xprime){
+g <- function(x,xprime){ ## transition density x[t+1] | x[t]
   (2*pi*Delta)^(-0.5) * exp(-(xprime - (1-Delta)*x)^2/(2*Delta))
 }
-q <- function(x,xprime){ ## proposal distribution for sampling p
-  p(x,xprime)
-}
 
-phi <- function(x, yt){
-  (2*pi*Delta)^(-0.5) * exp(-(yt-x)^2/(2*sigma^2))
-}
-g <- function(x,yt){
-  phi(x, yt)
+p <- function(y,x){ ## emission density y[t] | x[t]
+  (2*pi)^(-0.5)*sigma^(-1) * exp(-(y-x)^2/(2*sigma^2))
 }
 
 data <- sim.data(tmax, Delta, sigma)
@@ -40,19 +35,22 @@ y <- data$y
 plot(x, type='l', col=2)
 lines(y, col=1)
 
-stdSMC <- function(N, n.step=length(y), y){
+stdSMC <- function(N, y, n.step=length(y)-1){
   A <- matrix(NA, nrow=n.step, ncol=N)
-  x <- rnorm(N) ## initial state
-  w <- g(x, y[1]) ## calculate weights
-  w <- w/sum(w) ## normalise weights
-  for (t in 1:n.step){
-    A[t,] <- sample(1:N, N, prob=w, replace=T) ## choose the parent of each offspring
-    x <- x[a] ## resample the particles
-    x <- K(x) ## propagate particles
-    w <- g(x,y[t+1]) ## caluculate weights
-    w <- w/sum(w) ## normalise weights
+  X <- matrix(NA, nrow=n.step+1, ncol=N)
+  W <- matrix(NA, nrow=n.step+1, ncol=N)
+  X[1,] <- rnorm(N) ## initial state
+  w <- p(y[1], X[1,]) ## calculate weights
+  W[1,] <- w/sum(w) ## normalise weights
+  for (t in 2:(n.step+1)){
+    A[t-1,] <- sample(1:N, N, prob=w, replace=T) ## choose the parent of each offspring
+    x <- X[t-1,A[t-1,]] ## resample the particles
+    X[t,] <- rnorm(N, (1-Delta)*X[t-1,], Delta^(0.5)) ## propagate particles
+    w <- p(y[t], X[t,]) ## caluculate weights
+    W[t,] <- w/sum(w) ## normalise weights
+    print(t)
   }
-  list(x=x,w=w,A=A)
+  list(positions=X,weights=W,ancestry=A)
 }
 
 foo <- stdSMC(N,y)
