2ee7da060d696431736c5c18097c570c982121e9
suziebrown <suzie.brown42@gmail.com>
2018-05-24 11:56:06 +0100

    Description of simulation results

---
+++ Sim_data/n_16/readme.R
@@ -0,0 +1,10 @@
+## values used for n_16 data
+
+n_leaves <- 16
+n_particles_vals <- seq(from = 256, to = 4096, by = 256)
+n_reps <- 100
+delta <- 0.1
+sigma <- 0.1
+
+# immortal line generated as RTS smoother MAP + 0,1,2,3*SD
+# same observations used for all
