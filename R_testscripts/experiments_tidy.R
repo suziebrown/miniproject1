f6028faa558742d0c9bd7f41c78a42bac255291f
suziebrown <suzie.brown42@gmail.com>
2018-06-01 15:23:02 +0100

    updated to new (written from scratch) RTS smoother

--- R_testscripts/experiments_tidy.R
+++ R_testscripts/experiments_tidy.R
@@ -164,13 +164,28 @@ ou_initial_sam <- function(n_particles, delta, sigma) {
   rnorm(n_particles)
 }
 
-ou_rts_smooth <- function(observations, delta, sigma) {
+ou_rts_smooth <- function(observations, delta, sigma, prior_mean = 0, prior_var = 1) {
   n_obs <- length(observations) - 1
 
-  ## run Kalman filter
-  kalman <- KalmanSmooth(observations, mod=list(T = 1 - delta, Z = 1, h = sigma ^ 2, V = delta, a = 0, P = 1, Pn = 1))
-  kalman_mean <- kalman$smooth
-  kalman_var <- kalman$var[,,1]
+  ## initialise variables
+  kal_mean <- numeric(n_obs + 1) ## states x(t|t) after time & measurement updates
+  kal_var <- numeric(n_obs + 1)
+  kal_mean_tmp <- numeric(n_obs + 1) ## 'tmp' = states x(t|t-1) after time update and before measurement update
+  kal_var_tmp <- numeric(n_obs + 1)
+
+  ## Kalman filter recursion
+  kal_mean_tmp[1] <- prior_mean
+  kal_var_tmp[1] <- prior_var
+  for (t in 1:(n_obs)) {
+    ## measurement update:
+    kal_mean[t] <- kal_mean_tmp[t] + kal_var_tmp[t] * (observations[t] - kal_mean_tmp[t]) / (kal_var_tmp[t] + sigma ^ 2)
+    kal_var[t] <- kal_var_tmp[t] - kal_var_tmp[t] ^ 2 / (kal_var_tmp[t] + sigma ^ 2)
+    ## time update:
+    kal_mean_tmp[t + 1] <- (1 - delta) * kal_mean[t]
+    kal_var_tmp[t + 1] <- delta + kal_var[t] * (1 - delta) ^ 2
+  }
+  kal_mean[n_obs + 1] <- kal_mean_tmp[n_obs + 1] + kal_var_tmp[n_obs + 1] * (observations[n_obs + 1] - kal_mean_tmp[n_obs + 1]) / (kal_var_tmp[n_obs + 1] + sigma ^ 2)
+  kal_var[n_obs + 1] <- kal_var_tmp[n_obs + 1] - kal_var_tmp[n_obs + 1] ^ 2 / (kal_var_tmp[n_obs + 1] + sigma ^ 2)
 
   ## initialise variables
   rts_mean <- numeric(n_obs + 1)
@@ -179,15 +194,15 @@ ou_rts_smooth <- function(observations, delta, sigma) {
   g <- numeric(n_obs)
 
   ## final state
-  rts_mean[n_obs + 1] <- kalman_mean[n_obs + 1]
-  rts_var[n_obs + 1] <- kalman_var[n_obs + 1]
+  rts_mean[n_obs + 1] <- kal_mean[n_obs + 1]
+  rts_var[n_obs + 1] <- kal_var[n_obs + 1]
 
   ## RTS smoother recursion
   for (t in n_obs:1) {
-    pbar[t + 1] <- ((1 - delta) ^ 2) * kalman_var[t] + delta
-    g[t] <- kalman_var[t] * (1 - delta) / pbar[t + 1]
-    rts_mean[t] <- kalman_mean[t] + g[t] * (rts_mean[t + 1] - (1 - delta) * kalman_mean[t])
-    rts_var[t] <- kalman_var[t] + (g[t] ^ 2) * (rts_var[t + 1] - pbar[t + 1])
+    pbar[t + 1] <- ((1 - delta) ^ 2) * kal_var[t] + delta
+    g[t] <- kal_var[t] * (1 - delta) / pbar[t + 1]
+    rts_mean[t] <- kal_mean[t] + g[t] * (rts_mean[t + 1] - (1 - delta) * kal_mean[t])
+    rts_var[t] <- kal_var[t] + (g[t] ^ 2) * (rts_var[t + 1] - pbar[t + 1])
   }
 
   list(mean = rts_mean, variance = rts_var)
